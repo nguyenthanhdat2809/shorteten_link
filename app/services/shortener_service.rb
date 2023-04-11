a27e53eac4952287d3a8c2c036f41fd362bc8c23
Nguyen Thanh Dat <nguyenthanhdat12c@gmail.com>
2023-04-11 07:01:44 -0700

    refactor lookup code

--- app/services/shortener_service.rb
+++ app/services/shortener_service.rb
@@ -1,13 +1,27 @@
 require "digest/sha2"
 
 class ShortenerService
-	attr_reader :url
+	attr_reader :url, :link_model
 
-	def initialize(url)
+	def initialize(url, link_model = Link)
 		@url = url
+		@link_model = link_model
+	end
+
+	def generate_short_link
+		link_model.create(original_url: url, lookup_code: lookup_code)
 	end
 
 	def lookup_code
-		Digest::SHA256.hexdigest(url)[0..6]
+		loop do
+		  code = get_fresh_code
+		  break code unless link_model.exists?(lookup_code: code)
+		end
+	end
+
+	private
+
+	def get_fresh_code
+		SecureRandom.uuid[0..6]
 	end
 end
