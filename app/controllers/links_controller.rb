a5889814b06401f54dcee17e57bb2ad3118aa854
nguyenthanhdat2809 <nguyenthanhdat12c@gmail.com>
2023-04-13 09:31:57 -0700

    add view short link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,12 @@ class LinksController < ApplicationController
 		original_url = params[:link][:original_url]
 		shortener = ShortenerService.new(original_url)
 		@link = shortener.generate_short_link
+
+		if @link.persisted?
+			respond_to :js
+		else
+			render "error.js.erb"
+		end
 	end
 
 	private
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -9,6 +9,14 @@ RSpec.describe Link, type: :model do
     expect(link.valid?).to be(true)
   end
 
+  it "is invalid if the URL is not formatted properly" do
+    link = Link.new(
+      original_url: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      lookup_code: "1234568"
+    )
+    expect(link.valid?).to be(false)
+  end
+
   it "is valid if it does not have a lookup code" do
     link = Link.new(
       original_url: "https://viblo.asia/p/cac-cau-hoi-phong-van-ruby-on-rails-developer-phan-1-WAyK8O9m5xX"
